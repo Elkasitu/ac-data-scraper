4005b727cc2ee019f5fb49de239c7a9e149c6a5d
Adrian Torres <adt@odoo.com>
2020-03-31 20:22:22 +0200

    Make the scrape script more generic
    This new iteration uses `resources.json` to get endpoint / table xpath
    information instead of hardcoding it.
    
    It's more generic and capable of handling multiple games and resources
    as well as capability to add new games / resources easily.
    
    Additionally, there's a well defined directory structure now

--- scraper.py
+++ scraper.py
@@ -1,66 +1,57 @@
+import json
 import requests
 
 from lxml import etree
+from pathlib import Path
 
 
 BASE_URL = "https://animalcrossing.fandom.com/wiki/"
-ENDPOINTS = {
-    'fish': 'Fish_(New_Horizons)',
-}
-HEADER_XPATH = "//div[@title='Northern Hemisphere']/table[@class='roundy']//th"
-TABLE_XPATH = "//div[@title='{hemisphere}']/table[@class='roundy']/tr/td/table"
-HEMISPHERES = ["Northern Hemisphere", "Southern Hemisphere"]
 
 
-def get_raw(resource):
-    return requests.get(BASE_URL + ENDPOINTS[resource]).text
+def get_resources():
+    with open("resources.json", "r") as f:
+        return json.load(f)
 
 
-def parse(raw):
-    return etree.HTML(raw)
+def init_directories():
+    Path('./images/').mkdir(exist_ok=True)
+    Path('./data/').mkdir(exist_ok=True)
 
 
-def get_table(parsed, hemisphere=HEMISPHERES[0]):
-    return parsed.xpath(TABLE_XPATH.format(hemisphere=hemisphere))[0]
+def get_table(parsed, table_xpath):
+    return parsed.xpath(table_xpath)[0]
 
 
 def get_rows(table):
     return table.iterchildren(tag='tr')
 
 
-def get_headers(parsed):
-    return [col.text.strip().lower() for col in parsed.xpath(HEADER_XPATH)]
-
-
-def save_image(img):
+def save_image(img, image_path):
     url = img.get('src')
     name = img.get('data-image-key')
     r = requests.get(url, stream=True)
     if r.status_code == 200:
-        with open(f'images/{name}', 'wb') as f:
+        with image_path.joinpath(name).open('wb') as f:
             for chunk in r:
                 f.write(chunk)
         return name
     raise ValueError("Could not process image %r" % img)
 
 
-def get_fish():
-    parsed = parse(get_raw('fish'))
-    headers = get_headers(parsed)
-    rows = get_rows(get_table(parsed))
-    fishes = etree.Element("fishes")
+def build_tree(resource, image_path):
+    parsed = etree.HTML(requests.get(BASE_URL + resource['endpoint']).text)
+    rows = get_rows(get_table(parsed, resource['table_xpath']))
+    record_list = etree.Element("RecordList")
 
+    headers = [col.text.strip().lower() for col in next(rows).iterchildren(tag='th')]
     for row in rows:
-        if row.find('th') is not None:
-            # skip the header since we already parsed it
-            continue
-        fish = etree.Element("fish")
+        record = etree.Element("Record")
         for header, col in zip(headers, row):
             el = etree.Element(header.replace(' ', '_'))
 
             img = col.xpath('descendant::img[last()]')
-            if img:
-                name = save_image(img[0])
+            if img and header == 'image':
+                name = save_image(img[0], image_path)
                 el.text = name
             else:
                 text = max(col.xpath("descendant::text()")).strip()
@@ -69,12 +60,27 @@ def get_fish():
                 elif text == '-':
                     text = 'false'
                 el.text = text
-            fish.append(el)
-        fishes.append(fish)
-    return fishes
+            record.append(el)
+        record_list.append(record)
+    return record_list
+
+
+def main():
+    init_directories()
+    resources = get_resources()
+    for game in resources:
+        game_resources = resources[game]
+        image_path = Path(f'./images/{game}/')
+        data_path = Path(f'./data/{game}/')
+        image_path.mkdir()
+        data_path.mkdir()
+
+        for resource in game_resources:
+            res = game_resources[resource]
+            tree = build_tree(res, image_path)
+            with data_path.joinpath(resource + '.xml').open('wb') as f:
+                f.write(etree.tostring(tree, pretty_print=True))
 
 
 if __name__ == '__main__':
-    fishes = get_fish()
-    with open("fishes.xml", "wb") as f:
-        f.write(etree.tostring(fishes, pretty_print=True))
+    main()
